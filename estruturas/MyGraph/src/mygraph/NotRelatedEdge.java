9111ad7e016d42167a447f5022795ccab96e4a21
Felipe Pontes <felipemfpontes@gmail.com>
2017-12-05 09:45:20 -0300

    :sparkles: Add Graph

---
+++ estruturas/MyGraph/src/mygraph/NotRelatedEdge.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygraph;
+
+/**
+ *
+ * @author felipe
+ */
+public class NotRelatedEdge extends Exception {
+    
+}
